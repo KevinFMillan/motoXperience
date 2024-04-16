0d45ac31a0196ecc35ec2c96938d7aeb59042533
KevinFMillan <kevinat0627@gmail.com>
2024-04-16 07:22:44 -0500

    inicio

---
+++ app/src/main/java/com/example/motoxperience/InicioActivity.kt
@@ -0,0 +1,27 @@
+package com.example.motoxperience
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+        val btnIngreso = findViewById<AppCompatButton>(R.id.btnIngresar)
+        btnIngreso.setOnClickListener {
+            Log.i("motoxperience","se ingreso con exito")
+        }
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_inicio)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
